dc07b6c696a27f9d7f6b80ea4fe7e1e011bf1853
calosbol <116382767+calosbol@users.noreply.github.com>
2022-10-21 16:42:43 -0300

    Update webapp01.py

--- webapp01.py
+++ webapp01.py
@@ -5,13 +5,13 @@ from PIL import Image
 
 image01 = Image.open('moda-dopamina-dressing-tendencia.jpg')
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Bem vindo, EMEF/EJA Edson Luis Lima Souto!")
+st.title("kjmc Modas!")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Cabeçalho")
+st.header("are fashion")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
+st.subheader("clique abaixo para mais informações")
 
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
